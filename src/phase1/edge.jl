acf1dd3d080bcc20b54e33bf41c4d23abee7237d
Octave Claich <octave.claich@gmail.com>
2024-09-17 17:10:21 -0400

    Creation of edge type

---
+++ src/phase1/edge.jl
@@ -0,0 +1,19 @@
+import Base.show
+
+abstract type AbstractEdge{T} end
+
+mutable struct Edge{T} <: AbstractEdge{T}
+    node1::Node{T}
+    node2::Node{T}
+    weight::float
+end
+
+show_node1(edge::AbstractEdge) = edge.node1
+
+show_node2(edge::AbstractEdge) = edge.node2
+
+show_weight(edge::AbstractEdge) = edge.weight
+
+function show(edge::AbstractEdge)
+    println("Edge between ", show_node1(edge), " and ", show_node2(edge), ", weight = ", show_weight(edge))
+end
