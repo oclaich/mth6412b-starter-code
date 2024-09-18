33a8cd3e56aa5546b82c087ff7cd57ada0955476
Octave Claich <octave.claich@gmail.com>
2024-09-18 09:11:24 -0400

    Prise en compte des edges par la structure Graph

--- src/phase1/graph.jl
+++ src/phase1/graph.jl
@@ -17,11 +17,18 @@ Attention, tous les noeuds doivent avoir des données de même type.
 mutable struct Graph{T} <: AbstractGraph{T}
   name::String
   nodes::Vector{Node{T}}
+  edges::Vector{Edge{T}}
 end
 
 """Ajoute un noeud au graphe."""
 function add_node!(graph::Graph{T}, node::Node{T}) where T
   push!(graph.nodes, node)
+  graph
+end
+
+"""Ajoute une arête au graphe."""
+function add_edge!(graph::Graph{T}, edge::Edge{T}) where T
+  push!(graph.edges, edge)
   graph
 end
 
@@ -37,10 +44,20 @@ nodes(graph::AbstractGraph) = graph.nodes
 """Renvoie le nombre de noeuds du graphe."""
 nb_nodes(graph::AbstractGraph) = length(graph.nodes)
 
+"""Renvoie la liste des arêtes du graphe."""
+edges(graph::AbstractGraph) = graph.edges
+
+"""Renvoie le nombre d'arêtes du graphe."""
+nb_edges(graph::AbstractGraph) = length(graph.edges)
+
 """Affiche un graphe"""
 function show(graph::Graph)
   println("Graph ", name(graph), " has ", nb_nodes(graph), " nodes.")
   for node in nodes(graph)
     show(node)
+  end
+  println("Graph ", name(graph), " has ", nb_edges(graph), " edges.")
+  for edge in edges(graph)
+    show(edge)
   end
 end
