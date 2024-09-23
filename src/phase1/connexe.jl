5c0ec0d3ff253a6c2bfeb978e58d44c3eb453789
Octave Claich <octave.claich@gmail.com>
2024-09-23 09:52:39 -0400

    Ajout fonction change_parent

--- src/phase1/connexe.jl
+++ src/phase1/connexe.jl
@@ -1,10 +1,21 @@
 import Base.show
 
-abstract type AbstractConnexe{T} end
+abstract type AbstractCompConnexe{T} end
 
-mutable struct Connexe <: AbstractConnexe{T}
-
-    nodes::Vector{Node{T}}
-    parents::Vector{Node{T}}
+mutable struct CompConnexe{T} <: AbstractCompConnexe{T}
+    name::String
+    parent::Node{T}
+    child::Node{T}
 end
+
+"""Renvoie le nom de la composante connexe."""
+name(comp::AbstractCompConnexe) = comp.name
+
+""" Fonction pour changer le parent d'un noeud. """
+
+function change_parent(comp::AbstractCompConnexe,node::Node{T})
+    comp.parent = node
+    
+end
+
 
