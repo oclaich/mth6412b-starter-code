a1b3295c1f19e5c1b9994dd588a326358bbbfd4f
Octave Claich <octave.claich@gmail.com>
2024-09-30 15:38:17 -0400

    Changements sur la structure connexe à faire vérifier en labo, début de kruskal

--- src/phase1/connexe.jl
+++ src/phase1/connexe.jl
@@ -1,21 +1,44 @@
 import Base.show
+include("graph.jl")
+include("edge.jl")
+include("node.jl")
 
-abstract type AbstractCompConnexe{T} end
+abstract type AbstractConnexeElement{T} end
 
-mutable struct CompConnexe{T} <: AbstractCompConnexe{T}
+mutable struct ConnexeElement{T} <: AbstractConnexeElement{T}
     name::String
     parent::Node{T}
     child::Node{T}
 end
 
+abstract type AbstractCompConnexe{T} end
+
+mutable struct CompConnexe{T} <: AbstractCompConnexe{T}
+    comp::Vector{ConnexeElement}
+end
+
 """Renvoie le nom de la composante connexe."""
 name(comp::AbstractCompConnexe) = comp.name
 
-""" Fonction pour changer le parent d'un noeud. """
+"""Renvoie la taille de la composante connexxe"""
+length(comp::AbstractCompConnexe) = length(comp)
 
-function change_parent(comp::AbstractCompConnexe,node::Node{T})
-    comp.parent = node
-    
+"""Constructeur d'un élément de composante connexe"""
+function ConnexeElement(node::Node{T}) where {T}
+    name = node.name
+    parent = node
+    child = node
+    ConnexeElement(name,parent,child)
 end
 
+"""Constructeur d'une composante connexe"""
+function CompConnexe()
+    comp=ConnexeElement[]
+    CompConnexe(comp)
+end
+
+"""Changer le parent d'un élément de composante connexe"""
+function change_parent(element::ConnexeElement{T},parent::Node{T}) where {T}
+    element.parent=parent
+end
 
---
+++ src/phase1/kruskal.jl
@@ -0,0 +1,7 @@
+import Base.show
+include("connexe.jl")
+
+function  kruskal(graph::Graph{T}) where {T}
+
+    
+end
